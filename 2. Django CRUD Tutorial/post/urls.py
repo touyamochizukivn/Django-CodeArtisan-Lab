612933ac8ab66a426e6f4aadf00c8d20d0dec565
望月冬夜 <touyamochizukivn@gmail.com>
2022-09-25 05:18:57 +0700

    finish

--- 2. Django CRUD Tutorial/post/urls.py
+++ 2. Django CRUD Tutorial/post/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('add', views.add, name='add'),
     path('update/<int:id>', views.update, name='update'),
+    path('delete/<int:id>', views.delete, name='delete'),
 ]
--- 2. Django CRUD Tutorial/post/views.py
+++ 2. Django CRUD Tutorial/post/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from . import models
 
 
 def home(request):
-    return render(request, 'home.html')
+    posts = models.Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
     
 def add(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        detail = request.POST['detail']
+        models.Post.objects.create(title=title, detail=detail)
+        messages.success(request, 'Data has been added')
     return render(request, 'add.html')
 
 def update(request, id):
-    return render(request, 'update.html')
+    if request.method == 'POST':
+        title = request.POST['title']
+        detail = request.POST['detail']
+        post = models.Post.objects.filter(id=id).update(title=title, detail=detail)
+        messages.success(request, 'Data has been updated')
+    post = models.Post.objects.get(id=id)
+    return render(request, 'update.html', {'post': post})
+
+def delete(request, id):
+    models.Post.objects.filter(id=id).delete()
+    messages.success(request, 'Data has been deleted')
+    return redirect('/')
