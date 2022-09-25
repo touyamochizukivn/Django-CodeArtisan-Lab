a5526933992942cb1898b4339238a79aacd531f8
望月冬夜 <touyamochizukivn@gmail.com>
2022-09-25 14:19:56 +0700

    Add models

--- 3. Django News Website/main/admin.py
+++ 3. Django News Website/main/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from . import models
 
 
+class AdminNews(admin.ModelAdmin):
+    list_display = ('title', 'category', 'add_time')
+
+class AdminComment(admin.ModelAdmin):
+    list_display = ('news', 'email', 'comment', 'status')
+
 admin.site.register(models.Category)
+admin.site.register(models.News, AdminNews)
+admin.site.register(models.Comment, AdminComment)
--- 3. Django News Website/main/models.py
+++ 3. Django News Website/main/models.py
@@ -12,3 +12,29 @@ class Category(models.Model):
         verbose_name_plural = "Categories"
         verbose_name = "Category"
 
+
+class News(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='imgs/')
+    detail = models.TextField()
+    add_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "News"
+        verbose_name = "News"
+
+
+class Comment(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    comment = models.TextField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.comment
+    
