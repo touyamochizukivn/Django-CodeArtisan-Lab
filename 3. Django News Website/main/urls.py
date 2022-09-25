373999ff0913c8965190c38f66d77d91e11ec962
望月冬夜 <touyamochizukivn@gmail.com>
2022-09-25 15:09:58 +0700

    Templates

---
+++ 3. Django News Website/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('all-news', views.home, name='all-news'),
+    path('all-category', views.home, name='all-category'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- 3. Django News Website/main/views.py
+++ 3. Django News Website/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+
+def home(request):
+    context = {
+        'first_news': models.News.objects.first(),
+        'three_news': models.News.objects.all()[1:3],
+        'three_categories': models.Category.objects.all()[0:2],
+    }
+    return render(request, 'home.html', context)
