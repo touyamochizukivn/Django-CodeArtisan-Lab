fddf22e594c08185852f044bee1386796c8ce0cf
望月冬夜 <touyamochizukivn@gmail.com>
2022-09-27 04:38:05 +0700

    Custom url

--- 3. Django News Website/main/views.py
+++ 3. Django News Website/main/views.py
@@ -7,7 +7,7 @@ from . import models
 def home(request):
     context = {
         'first_news': models.News.objects.first(),
-        'three_news': models.News.objects.all()[1:3],
+        'three_news': models.News.objects.all(),
         'three_categories': models.Category.objects.all()[0:2],
     }
     return render(request, 'home.html', context)
