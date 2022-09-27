a0163665b0aca6d66893dd810839ad1348955a19
望月冬夜 <touyamochizukivn@gmail.com>
2022-09-27 11:25:38 +0700

    register

--- 4.Django Advanced Tutorial/main/urls.py
+++ 4.Django Advanced Tutorial/main/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('student', views.student, name='student'),
+    path('accounts/register', views.register, name='register')
 ]
--- 4.Django Advanced Tutorial/main/views.py
+++ 4.Django Advanced Tutorial/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 from . import forms
@@ -7,20 +8,30 @@ from . import forms
 def home(request):
     form = forms.EmployeeForm
     if request.method == 'POST':
-        empForm = forms.EmployeeForm(request.POST)
-        if empForm.is_valid():
-            empForm.save()
+        form = forms.EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
             messages.success(request, 'Data has been added')
             return redirect('/')
-    return render(request, 'home.html', {'form': form,})
+    return render(request, 'home.html', {'form': form, })
 
 
 def student(request):
     form = forms.StudentForm
     if request.method == 'POST':
-        stuForm = forms.StudentForm(request.POST)
-        if stuForm.is_valid():
-            stuForm.save()
+        form = forms.StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
             messages.success(request, 'Data has been added')
             return redirect('/student')
-    return render(request, 'student.html', {'form': form})
+    return render(request, 'student.html', {'form': form, })
+
+
+def register(request):
+    form = UserCreationForm
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'User has been registered')
+    return render(request, 'registration/register.html', {'form': form, })
