8d5dc624b1d1d5ca373502a507b239927e3926f1
望月冬夜 <touyamochizukivn@gmail.com>
2022-09-28 11:45:54 +0700

    password reset

--- 4. Django Advanced Tutorial/config/settings.py
+++ 4. Django Advanced Tutorial/config/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
